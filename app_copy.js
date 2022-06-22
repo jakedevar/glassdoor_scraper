b3fb68f35d8724658e25285c6b4ada824d3471ec
jakedevar <devarennesjacob@gmail.com>
2022-06-22 16:55:09 -0500

    from desktop

--- app_copy.js
+++ app_copy.js
@@ -1,130 +1,185 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs')
+// this var below was slapdash but closes the browser at the end
+let brows = null
 
-let browser;
-let page;
-let listings;
-
+// this operates the whole thing in reality, this opens the page, logs in, and then begins the process for the navigation class 
 class SetUp {
-
-}
-async function setUp() {
-
-	browser = await puppeteer.launch({headless:false, devtools:false});
-	page = await browser.newPage()
-
-	await page.goto('https://www.glassdoor.com/Job/software-engineer-jobs-SRCH_KO0,17.htm?context=Jobs&clickSource=searchBox')
-
-	await page.$(".react-job-listing, .css-bkasv9, .eigr9kq0").then(x => x.click()).catch(x => console.log('nah there was an error bruh'))
-	page.waitForSelector('.jaCreateAccountEmailSignUpButton').then(x => x.click()).catch(() => console.log('fuuuuuck'))
-	await page.waitForSelector('.css-w3qhip, .eb2o9h0').then(x => x.click()).catch(() => console.log('fuuuuuck'))
-
-	await page.type('.css-1kmcde, .e1h5k8h92', 'zakquerygotem@gmail.com').then(() => null).catch(() => console.log('email not logged'))
-	await page.keyboard.press('Enter', {delay: 500}).then(() => null).catch(() => console.log('enter unsuccessfull'))
-	await page.$('.css-w3qhip, .eb2o9h0').then(x => x.click()).catch(() => console.log('click unsuccessfull'))
-	await page.type('.css-1kmcde, .e1h5k8h92', '10FoldJones').then(() => null).catch(x => console.log('type unsuccessfull'))
-	await page.keyboard.press('Enter', {delay: 1000}).then(() => null).catch(() => console.log('enter unsuccessfull'))
-
-	let prom = new Promise((resolve, reject) => {
-		page.on('load', () => {
-			page.$$('.evpplnh1').then(x => x[8].click()).catch(() => console.log('its not happenin'))
-			resolve()
-		})
-	})
-	await prom.then(() => null).catch(() => console.log('promise not wokring'))
-
-}
-const jobs = [];
-let counter = 0;
-let jobPage = 0;
-let terms = ['javascript', 'elm', 'typescript', 'scala', 'python', ' go ', 'ruby', 
-'swift', 'java', ' c ', 'rust', 'html', 'css', 'sql', 'nosql', 'postgres', 'c\#',  'perl', ' r ', 'node\.js', 'git', 
-'vue', 'angular', 'django', 'ruby on rails', 'laravel', 'asp\.net', 'express', 'spring', 'ember', 'react', 'meteor', 'agile', 'kotlin', 'php',
-'apache', 'mysql', 'linux', '\.net', 'graphql', 'cassandra', 'hadoop', 'flutter', 'iis', 'sql server', ]
-;
-
-async function objectMaker(listings) {
-	let job = {};
-	job.id = counter + 1;
-	job.jobPage = jobPage
-	let jobText = await listings[counter].getProperty('textContent').then(x => x.jsonValue()).catch(() => console.log('not working this one is'))
-	let sals = jobText.match(/\$\d+.*\$\d+/)
-	if (sals) {
-		sals = sals[0].split(' - ')
-		minSal = sals[0];
-		maxSal = sals[1];
-		job.minSal = minSal;
-		job.maxSal = maxSal;
+	constructor() {
+		return this
 	}
-	await page.waitForSelector(".jobDescriptionContent").then(() => null).catch(() => console.log('nope'))
-	let text = await page.$eval('.jobDescriptionContent', (jobDesc) => {
-		return jobDesc.textContent;
-	}).then((jobDesc) => jobDesc).catch(() => console.log('job desc not working'))
-	text = text.toLowerCase()
-	terms.forEach(term => {
-		if (text.match(term)) {
-			job[term] = text.match(term).length
-		}
-	})
-	// console.log(job)
-	jobs.push(job);
-}
-// css-1buaf54 pr-xxsm //money div
 
-function mainObj() {
-	let main = {};
-	jobs.forEach(listing => {
-		let keys = Object.keys(listing);
-		keys.forEach(key => {
-			main[key] = main[key] || 0;
-			main[key] += 1;
+	async initializePage() {
+		this.browser = await puppeteer.launch({headless:false, devtools:false});
+		brows = this.browser
+		this.page = await this.browser.newPage()
+		await this.page.goto('https://www.glassdoor.com/Job/software-engineer-jobs-SRCH_KO0,17.htm?context=Jobs&clickSource=searchBox')
+		await this.login()	
+	}	
+
+	async randomClick(time) {
+		setTimeout(async () => {
+			await this.page.waitForSelector(".react-job-listing, .css-bkasv9, .eigr9kq0").then(x => x.click()).catch(() => console.log('its not happenin'))
+		}, time)
+	}
+
+	// this operates the login sequence, its clunky but it should work MOST of the time (sometimes you have to restart it)
+	async login() {
+		await this.randomClick(2000)
+		setTimeout(async () => {
+			await this.page.waitForSelector('.jaCreateAccountEmailSignUpButton').then(x => x.click()).catch(() => console.log('click email for typing unsuccessfull'))
+		}, 3000)
+		await this.page.waitForSelector('.css-w3qhip, .eb2o9h0').then(x => x.click()).catch(() => console.log('click email for typing unsuccessfull'))
+		await this.page.type('.css-1kmcde, .e1h5k8h92', 'zakquerygotem@gmail.com').then(() => true).catch(() => console.log('typing email unsuccessfull'))
+		await this.page.keyboard.press('Enter', {delay: 500}).then(() => true).catch(() => console.log('enter unsuccessfull'))
+		await this.page.$('.css-w3qhip, .eb2o9h0').then(x => x.click()).catch(() => console.log('click unsuccessfull'))
+		await this.page.type('.css-1kmcde, .e1h5k8h92', '10FoldJones').then(() => true).catch(x => console.log('password type unsuccessfull'))
+		await this.page.keyboard.press('Enter', {delay: 1000}).then(() => true).catch(() => console.log('enter unsuccessfull'))
+		await this.randomClick(2000)
+	}
+
+	async startSetup() {
+		await this.initializePage()
+		setTimeout(() => {
+			new pageNavigation(this.page)
+		}, 2000)
+	}
+}
+
+// I tried making somethings private but I don't really know how much good it will do 
+// CreateJobs makes the array for all the jobs as well as the main object 
+class CreateJobs {
+	#jobsArray = []
+	#job = {}
+	#terms = ['javascript', 'elm', 'typescript', 'scala', 'python', ' go ', 'ruby', 'swift', 'java', ' c ', 'rust', 'html', 'css', 'sql', 'nosql', 'postgres', 'c#',  'perl', ' r ', 'node\.js', 'git', 
+'vue', 'angular', 'django', 'ruby on rails', 'laravel', 'asp\.net', 'express', 'spring', 'ember', 'react', 'meteor', 'agile', 'kotlin', 'php',
+'apache', 'mysql', 'linux', '\.net', 'graphql', 'cassandra', 'hadoop', 'flutter', 'iis', 'sql server', 'flask', 'spring', 'sqlite', 'redis', 'mariadb', 'firebase', 'elasticsearch', 'nginx', 'amazon rds', 'rocksdb', 'beringei', 'memcached',
+'tornado', 'hbase', 'objective-c', 'thrift', 'blazor', 'elixir', 'erlang', ' raspberrypi', 'cm4', 'hmi', 'windows', 'modbus', 'bacnet', 'ethernet', ' ip ', 'rs485', 'usb', 'host', 'bluetooth', 'azure', 'jquery', 'visual', 'devops', 'scss', ' ux ', ' ui ', 
+'arm', ' aws ', 'oauth', 'openid', ' saml ', ' spa ', ' git ', 'xamarin', 'cosmosdb', 'rxjs', 'redux', 'sdlc', 'cloud', ' mvc ', 'vb\.net', 'bootstrap', 's3', 'ec2', ' qa ', 'soap', 'aurora', 'docker', ' qml ', ' qt5 ', 'cmake', 'mqtt', 'qtest', 'dotnetcore', 'tsql', 'nextjs', 
+'ionic', ' ai ', 'machine learning', 'scrum', 'providex', 'bbx', 'j2ee', 'unix', ' foundation ', ' css3 ', ' sass ', 'github', 'bitbucket',];
+
+	constructor(page) {
+		this.page = page
+		return this
+	}
+
+	async createJobObj(listing) {
+		let jobText = await listing.getProperty('textContent').then(x => x.jsonValue()).catch(() => console.log('jobText unsuccessfull'))
+		this.#createSalary(jobText)
+		await this.page.waitForSelector(".jobDescriptionContent").then(() => null).catch(() => console.log('jobDescriptionContent selector did not appear'))
+		let text = await this.page.$eval('.jobDescriptionContent', (jobDesc) => {
+			return jobDesc.textContent;
+		}).then((jobDesc) => jobDesc).catch(() => console.log('jobDescriptionContent was not returned'))
+		text = text.toLowerCase()
+		this.#terms.forEach(term => {
+			if (text.match(term)) {
+				this.#job[term] = text.match(term).length
+			}
 		})
-	})
-	return main;
+		this.#jobsArray.push(this.#job);
+	}
+
+	async #createSalary(jobText) {
+		let sals = jobText.match(/\$\d+.*\$\d+/)
+		if (sals) {
+			sals = sals[0].split(' - ')
+			let minSal = sals[0];
+			let maxSal = sals[1];
+			this.#job.minSal = minSal;
+			this.#job.maxSal = maxSal;
+		}
+	}
+
+	// this is the averages and counts for everything in the jobsArray 
+	createMainObj() {
+		let main = {};
+		this.#jobsArray.forEach(listing => {
+			let keys = Object.keys(listing);
+			keys.forEach(key => {
+				main[key] = main[key] || 0;
+				main[key] += 1;
+			})
+		})
+		return main;
+	}
+
+	exposeJobsArray() {
+		return this.#jobsArray
+	}
 }
 
+// this navigates the page and clicks through the job listings and to the next page there should be only 30 pages traveled in total for 900 jobs
+class pageNavigation {
+	#listings = null
+	counter = 1
+	jobPage = 0
+	CreateJobs = null
 
-async function objectGet() {
-	listings = await page.$$(".react-job-listing, .css-bkasv9, .eigr9kq0").then(x => x).catch(x => console.log('nah there was an error bruh'));
-	console.log(listings)
-	let int = setInterval(async () => {
-		await listings[counter].click();
-		await objectMaker(listings)
-		counter += 1;
-		console.log(jobPage)
-		if (counter === listings.length) {
-			clearInterval(int);
-			counter = 0;
-			jobPage += 1
-			if (jobPage > 29) {
+	constructor(page) {
+		this.page = page
+		this.CreateJobs = new CreateJobs(this.page)
+		this.createInterval()
+	}
+
+	async #createListings() {
+		this.#listings = await this.page.$$(".react-job-listing").then(x => x).catch(x => console.log('listings creation unsuccessfull'));
+	}
+
+	async next() {
+		await this.page.$$('.css-g9i05x-svg, .e13qs2073-svg').then(x => x[1].click())
+	}
+
+	async createInterval() {
+		// interval was the only way I knew how to get a loop going without it iterating to fast.
+		await this.#createListings()
+		let int = setInterval(async () => {
+			await this.#listings[this.counter].click();
+			await this.CreateJobs.createJobObj(this.#listings[this.counter])
+			this.counter += 1;
+			console.log(this.jobPage)
+
+			// determine if to continue to next page or finish operation
+			if (this.counter === this.#listings.length && this.jobPage < 30) {
+				this.nextPageAndStart(int)
+			} else if (this.jobPage >= 29) {
 				const jobsData = {};
-				jobsData.termCounts = mainObj()
-				jobsData.jobsCollection = jobs
-				let date = new Date()
-				let string = await `glassdoor_${date.getMonth()}_${date.getDate()}_${date.getFullYear()}.json`
-			  await fs.writeFile(`data/${string}`, JSON.stringify(jobsData, null, 2), (error) => error ? console.log('file created successfully') : console.log(error));
+				jobsData.termCounts = this.CreateJobs.createMainObj()
+				jobsData.jobsCollection = this.CreateJobs.exposeJobsArray()
+				await this.writeFile(jobsData)
+				brows.close()
 				return;
 			}
-			await next();
-			setTimeout(async () => {
-				await page.on('load')
-				await objectGet();
-			}, 3000)
-		}
-	}, 3000)  
+		}, 3000)  
+	}
+
+	async writeFile(jobsData) {
+		let date = new Date()
+		let string = await `glassdoor_${date.getMonth() + 1}_${date.getDate()}_${date.getFullYear()}.json`
+		await fs.writeFile(`data/${string}`, JSON.stringify(jobsData, null, 2), (error) => error ? console.log('file created successfully') : console.log(error));
+	}
+
+	async nextPageAndStart(int) {
+		clearInterval(int);
+		this.counter = 0;
+		this.jobPage += 1
+		await this.next();
+		setTimeout(async () => {
+			await this.page.on('load')
+			await this.createInterval();
+		}, 3000)
+	}
 }
 
+// redundant but seemed like the right way to initiate app
+class App {
+	constructor() {
+		this.operation()
+	}
 
-
-
-async function next() {
-	await page.$$('.css-g9i05x-svg, .e13qs2073-svg').then(x => x[1].click())
+	async operation() {
+		let setup = new SetUp()
+		setup.startSetup()
+	}
 }
 
-async function app() {
-	await setUp().then(() => console.log('its happenin')).catch(() => console.log('whole thing not working'));
-	await objectGet().then(() => console.log('its also working')).catch(x => console.log('its also not working'));
-}
-
-
-app().then(() => console.log('its def happenin')).catch(() => console.log('whole thing def not working'))
+new App()
